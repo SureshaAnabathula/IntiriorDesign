1c0a8bc4d7bdb6e7f70d46be5eebfad58c1995e3
Suresh <suresh.anabathula999@gmail.com>
2023-12-11 15:39:46 +0000

    changes updated

--- app/src/main/java/com/suresh/interiordesigning/ItemDetialsActivity.kt
+++ app/src/main/java/com/suresh/interiordesigning/ItemDetialsActivity.kt
@@ -154,7 +154,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     }
             ) {
                 Text(
-                    text = "Place Order",
+                    text = " Place Order",
                     color = Color.Black,
                     textAlign = TextAlign.Center,
                     style = TextStyle(
